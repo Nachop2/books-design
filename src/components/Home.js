46d9431e3faf3b5f63641b519b76886b8e158935
Ignacio <italiyankin@gmail.com>
2024-03-11 08:41:20 +0000

    clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -73,139 +73,94 @@ const Home = () => {
             console.log(await response.json());
             setCards([]);
             fetchBooks();
-            console.log("book gaming");
         }
     }
 
-
-
-    const titles = [
-        { title: "Libros", icon: "clipboard-list" },
-    ];
-
-
-
-    const carouselItems = [
-        {
-            id: 1,
-            src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg',
-            alt: 'First slide',
-            caption: { title: 'First slide label', text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.' }
-        },
-        {
-            id: 2,
-            src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg',
-            alt: 'Second slide',
-            caption: { title: 'Second slide label', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
-        },
-        {
-            id: 3,
-            src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg',
-            alt: 'Third slide',
-            caption: { title: 'Third slide label', text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.' }
-        }
-    ];
-
     return (
-        <div>
-            {/* <MDBCarousel showIndicators showControls fade>
-                {carouselItems.map((item) => (
-                    <MDBCarouselItem key={item.id} itemId={item.id}>
-                        <img src={item.src} className='d-block w-100' alt={item.alt} />
-                        <MDBCarouselCaption>
-                            <h5>{item.caption.title}</h5>
-                            <p>{item.caption.text}</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                ))}
-            </MDBCarousel> */}
 
-            {titles.map((title, index) => (
-                <div className="mt-5" key={index}>
-                    <MDBCard>
-                        <MDBCardHeader>
-                            <MDBRow>
-                                <MDBCol className="col-2">
-                                    <MDBTypography tag='h3' className="text-center my-3">
-                                        <MDBIcon fas icon={title.icon} /> {title.title}
-                                    </MDBTypography>
-                                </MDBCol>
-                                <MDBCol className="d-flex " style={{ alignItems: "center" }}>
-                                    <form className="input-group my-2">
-                                        <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
-                                        <MDBBtn color="primary"><MDBIcon fas icon="search" /></MDBBtn>
-                                    </form>
-                                </MDBCol>
-                            </MDBRow>
-                        </MDBCardHeader>
-                        <MDBCardBody>
-                            <MDBCardText>
 
-                                <MDBRow className="row-cols-1 row-cols-md-12 g-4">
-                                    {cards.map((card, i) => (
-                                        <MDBCol key={i}>
+        <div className="mt-5">
+            <MDBCard>
+                <MDBCardHeader>
+                    <MDBRow>
+                        <MDBCol className="col-2">
+                            <MDBTypography tag='h3' className="text-center my-3">
+                                <MDBIcon fas icon={"book-open"} /> {"Libros"}
+                            </MDBTypography>
+                        </MDBCol>
+                        <MDBCol className="d-flex " style={{ alignItems: "center" }}>
+                            <form className="input-group my-2">
+                                <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
+                                <MDBBtn color="primary"><MDBIcon fas icon="search" /></MDBBtn>
+                            </form>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBCardHeader>
+                <MDBCardBody>
+                    <MDBCardText>
 
-                                            <MDBCard>
-                                                {/* <MDBRipple rippleTag='div' className='bg-image hover-zoom'>
+                        <MDBRow className="row-cols-1 row-cols-md-12 g-4">
+                            {cards.map((card, i) => (
+                                <MDBCol key={i}>
+
+                                    <MDBCard>
+                                        {/* <MDBRipple rippleTag='div' className='bg-image hover-zoom'>
                                                         <MDBCardImage src={card.image} alt={card.title} position="top" />
                                                         <MDBCardLink href={`/quiz/play/${card.id}`}>
                                                             <div className='mask'></div>
                                                         </MDBCardLink>
                                                     </MDBRipple> */}
 
-                                                <MDBCardBody>
+                                        <MDBCardBody>
 
-                                                    <MDBRow>
-                                                        <MDBCol>
-                                                            {/* href={`/quiz/play/${card.id}`} */}
-                                                            <MDBCardLink>
-                                                                <MDBCardTitle>{card.title}</MDBCardTitle>
-                                                            </MDBCardLink>
-                                                            {card.category_names.map(name => (
-                                                                <MDBBadge pill light color='primary' className="mb-3 me-1">
-                                                                    {name}
-                                                                </MDBBadge>
-                                                            ))}
-                                                            <MDBCardText>{card.text}</MDBCardText>
+                                            <MDBRow>
+                                                <MDBCol>
+                                                    {/* href={`/quiz/play/${card.id}`} */}
+                                                    <MDBCardLink>
+                                                        <MDBCardTitle>{card.title}</MDBCardTitle>
+                                                    </MDBCardLink>
+                                                    {card.category_names.map(name => (
+                                                        <MDBBadge pill light color='primary' className="mb-3 me-1">
+                                                            {name}
+                                                        </MDBBadge>
+                                                    ))}
+                                                    <MDBCardText>{card.text}</MDBCardText>
 
-                                                        </MDBCol>
-                                                        <MDBCol className="d-flex" style={{ justifyContent: "flex-end", alignItems: "center" }}>
-                                                            <MDBBadge pill light color='primary' className="p-2 me-2">
-                                                                Cantidad: {card.stock}
-                                                            </MDBBadge>
-                                                            <MDBBtn color="danger" className="ms-2" href="" onClick={() => handleBook(card.id, "sell", 1)}>
-                                                                <MDBIcon fas icon="dollar-sign" /> Vender
-                                                            </MDBBtn>
-                                                            <MDBDropdown>
-
-
-                                                                <MDBDropdownToggle color="success" className="ms-2" href="" ><MDBIcon fas icon="sign-in-alt" /> Añadir</MDBDropdownToggle>
-                                                                {/* onClick={() => handleBook(card.id, "add")} */}
-                                                                <MDBDropdownMenu>
-                                                                    {[...Array(15)].map((x, i) =>
-                                                                        <MDBDropdownItem link key={i + 1} onClick={() => handleBook(card.id, "add", i + 1)}>{i + 1}</MDBDropdownItem>
-                                                                    )}
-                                                                </MDBDropdownMenu>
-                                                            </MDBDropdown>
-                                                        </MDBCol>
-                                                    </MDBRow>
+                                                </MDBCol>
+                                                <MDBCol className="d-flex" style={{ justifyContent: "flex-end", alignItems: "center" }}>
+                                                    <MDBBadge pill light color='primary' className="p-2 me-2">
+                                                        Cantidad: {card.stock}
+                                                    </MDBBadge>
+                                                    <MDBBtn color="danger" className="ms-2" href="" onClick={() => handleBook(card.id, "sell", 1)}>
+                                                        <MDBIcon fas icon="dollar-sign" /> Vender
+                                                    </MDBBtn>
+                                                    <MDBDropdown>
 
 
-                                                </MDBCardBody>
+                                                        <MDBDropdownToggle color="success" className="ms-2" href="" ><MDBIcon fas icon="sign-in-alt" /> Añadir</MDBDropdownToggle>
+                                                        {/* onClick={() => handleBook(card.id, "add")} */}
+                                                        <MDBDropdownMenu>
+                                                            {[...Array(15)].map((x, i) =>
+                                                                <MDBDropdownItem link key={i + 1} onClick={() => handleBook(card.id, "add", i + 1)}>{i + 1}</MDBDropdownItem>
+                                                            )}
+                                                        </MDBDropdownMenu>
+                                                    </MDBDropdown>
+                                                </MDBCol>
+                                            </MDBRow>
 
-                                            </MDBCard>
-                                        </MDBCol>
-                                    ))}
-                                </MDBRow>
-                                <div className="d-flex justify-content-center mt-5">
-                                    <MDBCardLink href="/category/1">Ver más de "{title.title}"</MDBCardLink>
-                                </div>
-                            </MDBCardText>
-                        </MDBCardBody>
-                    </MDBCard>
-                </div>
-            ))}
+
+                                        </MDBCardBody>
+
+                                    </MDBCard>
+                                </MDBCol>
+                            ))}
+                        </MDBRow>
+
+                    </MDBCardText>
+                </MDBCardBody>
+            </MDBCard>
         </div>
+
     );
 }
 
