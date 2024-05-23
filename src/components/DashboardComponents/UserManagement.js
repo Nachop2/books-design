f5ade274dab50ae23fdf500da1202a2359968ac0
M2 <italiyankin@gmail.com>
2024-05-23 05:17:57 +0100

    improve user management

--- src/components/DashboardComponents/UserManagement.js
+++ src/components/DashboardComponents/UserManagement.js
@@ -27,14 +27,14 @@ const UserManagement = () => {
     const [basicModal, setBasicModal] = useState(false);
 
     const toggleOpen = () => setBasicModal(!basicModal);
-
+    console.log(formattedCurrentUserData);
     const navigate = useNavigate();
-    // const [users, setUsers] = useState([]);
-    const users = [
-        { username: 'RandomUser', image_src: 'https://mdbootstrap.com/img/new/avatars/1.jpg' },
-        { username: 'RandomUser2', image_src: 'https://mdbootstrap.com/img/new/avatars/2.jpg' },
-        { username: 'RandomUser3', image_src: 'https://mdbootstrap.com/img/new/avatars/3.jpg' }
-    ];
+    const [users, setUsers] = useState([]);
+    // const users = [
+    //     { username: 'RandomUser', image_src: 'https://mdbootstrap.com/img/new/avatars/1.jpg' },
+    //     { username: 'RandomUser2', image_src: 'https://mdbootstrap.com/img/new/avatars/2.jpg' },
+    //     { username: 'RandomUser3', image_src: 'https://mdbootstrap.com/img/new/avatars/3.jpg' }
+    // ];
     console.log(users);
     useEffect(() => {
         const fetchUsers = async (e) => {
@@ -59,7 +59,7 @@ const UserManagement = () => {
                 }
                 const userData = await response.json();
                 console.log(userData);
-                // setUsers(userData);
+                setUsers(userData);
             }
             catch (error) {
                 console.error('Error fetching users:', error);
@@ -67,71 +67,7 @@ const UserManagement = () => {
         }
 
         fetchUsers();
-    }, []);
-
-
-    const onView = (id) => {
-        // TODO - Crear una ruta para poder ver el perfil público de un usuario (además de una ruta en el back también)
-        navigate(`/user/${id}`);
-    }
-
-    const onModPromotion = async (id) => {
-        Swal.fire({
-            icon: "warning",
-            text: "AVISO: Está apunto de PROMOCIONAR a un usuario a moderador. ¿Está seguro de darle estos permisos?",
-            showCancelButton: true,
-            confirmButtonText: "Sí, estoy seguro",
-            confirmButtonColor: "green",
-            cancelButtonText: "Cancelar",
-            cancelButtonColor: "red"
-        }).then(result => {
-            if (result.isConfirmed) {
-                const csrfToken = document.cookie
-                    .split('; ')
-                    .find(cookie => cookie.startsWith('XSRF-TOKEN='))
-                    ?.split('=')[1];
-
-                const formData = new FormData();
-                formData.append('_method', 'PUT'); // Use method spoofing for Laravel: PUT doesn't work properly
-                formData.append('role', 'mod');
-
-                const promoteToMod = async (e) => {
-                    try {
-                        // TODO - Crear ruta para poder actualizar a un usuario (excepto el mismo con el que se está en sesión)
-                        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/${id}`, {
-
-                            method: 'POST',
-                            headers: {
-                                'Accept': 'application/json',
-                                'X-XSRF-TOKEN': decodeURIComponent(csrfToken), // Include the CSRF token in the headers
-                            },
-                            credentials: 'include', // Include cookies for the domain
-                            body: formData
-                        });
-
-                        if (!response.ok) {
-                            throw new Error("Failed to promote user to mod");
-                        }
-
-                        console.log("User promoted to mod successfully");
-                        await Swal.fire({
-                            icon: "success",
-                            text: "Usuario promocionado a moderador con éxito",
-                            timer: 3000
-                        }).then(result => {
-                            window.location.reload();
-                        });
-                    }
-                    catch (error) {
-                        console.error(error);
-                    }
-                };
-
-                promoteToMod();
-            }
-        });
-    }
-
+    }, [basicModal]);
 
     const onAdminPromotion = async (id) => {
         Swal.fire({
@@ -156,7 +92,7 @@ const UserManagement = () => {
                 const promoteToAdmin = async (e) => {
                     try {
                         // TODO - Crear ruta para poder actualizar a un usuario (excepto el mismo con el que se está en sesión)
-                        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/${id}`, {
+                        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/admin/user/${id}`, {
 
                             method: 'POST',
                             headers: {
@@ -210,7 +146,7 @@ const UserManagement = () => {
                 const deleteUser = async (e) => {
                     try {
                         // TODO - Crear ruta para poder eliminar a un usuario (excepto el mismo con el que se está en sesión)
-                        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/${id}`, {
+                        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/admin/user/${id}`, {
 
                             method: 'DELETE',
                             headers: {
@@ -248,7 +184,7 @@ const UserManagement = () => {
             <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex='-1'>
                 <MDBModalDialog>
                     <MDBModalContent>
-                        <Register></Register>
+                        <Register setBasicModal={setBasicModal}></Register>
                     </MDBModalContent>
                 </MDBModalDialog>
             </MDBModal>
@@ -265,65 +201,35 @@ const UserManagement = () => {
                                 <div className='d-flex justify-content-between align-items-center'>
                                     <div className='d-flex align-items-center'>
                                         <img
-                                            src={user.image_src}
+                                            src="https://mdbootstrap.com/img/new/avatars/1.jpg"
                                             alt='User image'
                                             style={{ width: '45px', height: '45px' }}
                                             className='rounded-circle'
                                         />
                                         <div className='ms-3'>
-                                            <p className='fw-bold mb-1'>{user.username}</p>
+                                            <p className='fw-bold mb-1'>{user.name}</p>
                                         </div>
                                     </div>
                                     <div className="ms-5">
-                                        <MDBBtn size='sm' rounded color='link' onClick={() => onView(user.id)}>
-                                            <MDBIcon fas icon="eye" />
-                                        </MDBBtn>
-
-                                        {/* Opciones para moderador */}
-                                        {formattedCurrentUserData.role === 'mod' && (
-                                            user.role === 'user' && (
-                                                <MDBBtn size='sm' rounded color='info' className="ms-2"
-                                                    onClick={() => onModPromotion(user.id)}>
-                                                    <MDBIcon fas icon="arrow-alt-circle-up" /> Moderador
-                                                </MDBBtn>
-                                            )
+                                        {!user.role == "admin" ? (
+                                            <MDBBtn size='sm' rounded color='primary' className="ms-2"
+                                                onClick={() => onAdminPromotion(user.id)}>
+                                                <MDBIcon fas icon="arrow-alt-circle-up" /> Administrador
+                                            </MDBBtn>
+                                        ) : (
+                                            null
                                         )}
 
-                                        {/* Opciones para administrador */}
-                                        {formattedCurrentUserData.role === 'admin' && (
-                                            user.role === 'user' ? (
-                                                <>
-                                                    <MDBBtn size='sm' rounded color='info' className="ms-2"
-                                                        onClick={() => onModPromotion(user.id)}>
-                                                        <MDBIcon fas icon="arrow-alt-circle-up" /> Moderador
-                                                    </MDBBtn>
-
-                                                    <MDBBtn size='sm' rounded color='primary' className="ms-2"
-                                                        onClick={() => onAdminPromotion(user.id)}>
-                                                        <MDBIcon fas icon="arrow-alt-circle-up" /> Administrador
-                                                    </MDBBtn>
-
-                                                    <MDBBtn size='sm' rounded color='danger' className="ms-2"
-                                                        onClick={() => onDelete(user.id)}>
-                                                        <MDBIcon fas icon="trash" />
-                                                    </MDBBtn>
-                                                </>
-                                            ) : (
-                                                user.role === 'mod' && (
-                                                    <>
-                                                        <MDBBtn size='sm' rounded color='primary' className="ms-2"
-                                                            onClick={() => onAdminPromotion(user.id)}>
-                                                            <MDBIcon fas icon="arrow-alt-circle-up" /> Administrador
-                                                        </MDBBtn>
-
-                                                        <MDBBtn size='sm' rounded color='danger' className="ms-2"
-                                                            onClick={() => onDelete(user.id)}>
-                                                            <MDBIcon fas icon="trash" />
-                                                        </MDBBtn>
-                                                    </>
-                                                )
-                                            )
+                                        {user.id == formattedCurrentUserData.id ? (
+                                            null
+                                        ) : (
+                                            <MDBBtn size='sm' rounded color='danger' className="ms-2"
+                                                onClick={() => onDelete(user.id)}>
+                                                <MDBIcon fas icon="trash" />
+                                            </MDBBtn>
                                         )}
+
+
                                     </div>
                                 </div>
                             </MDBListGroupItem>
---
+++ src/components/Register.jsx
@@ -0,0 +1,118 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {
+    MDBInput,
+    MDBCheckbox,
+    MDBBtn,
+    MDBIcon,
+    MDBCard,
+    MDBTypography,
+    MDBCardHeader, MDBCardBody, MDBCardText
+} from 'mdb-react-ui-kit';
+import Swal from "sweetalert2";
+
+const Register = ({ setBasicModal }) => {
+    const [errors, setErrors] = useState(null);
+    const navigate = useNavigate();
+    const register = async (e) => {
+        e.preventDefault();
+
+        const cookie = await fetch(`${process.env.REACT_APP_BACKEND_URL}/sanctum/csrf-cookie`, {
+            method: 'GET',
+            credentials: 'include'
+        });
+        const token = document.cookie
+            .split('; ')
+            .find(cookie => cookie.startsWith('XSRF-TOKEN='))
+            ?.split('=')[1];
+
+        console.log(token);
+        // Build formData object.
+
+        let formData = new FormData();
+        formData.append('name', document.querySelector("#username").value);
+        formData.append('email', document.querySelector("#email").value);
+        formData.append('password', document.querySelector("#password").value);
+        formData.append('password_confirmation', document.querySelector("#password_confirmation").value);
+
+
+        await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/admin/users`, {
+
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                //'Content-Type': 'application/json',
+                //'X-Requested-With': 'XMLHttpRequest',
+                'X-XSRF-TOKEN': decodeURIComponent(token), // Include the CSRF token in the headers
+            },
+            credentials: 'include', // Include cookies in the request
+            body: formData
+        }).then(async (res) => {
+            if (!res.ok) {
+                handleErrors(res.errors);
+                Swal.fire({
+                    icon: "error",
+                    title: "Hubo errores en la creacion del usuario ",
+                    showConfirmButton: true,
+                })
+            }
+            else {
+                Swal.fire({
+                    icon: "success",
+                    title: "El usuario se creo con éxito",
+                    showConfirmButton: true,
+                }).then(result => {
+                    if (result.isConfirmed) {
+                        setBasicModal(false)
+                    }
+                })
+                    .catch(error => console.error("Fucky wacky", error));
+            }
+        })
+
+    }
+
+    const handleErrors = (errors) => {
+        if (errors) {
+            setErrors(errors);
+        }
+    }
+
+
+
+    return (
+        // <div className="d-flex justify-content-center align-content-center mt-5">
+        <MDBCard>
+            <MDBCardHeader>
+                <MDBTypography tag='h3' className="my-3">Register</MDBTypography>
+            </MDBCardHeader>
+            <MDBCardBody>
+                <MDBCardText>
+                    <form id="formElement">
+                        {errors && (
+                            <div className="alert alert-danger" role="alert">
+                                <h6>Has cometido los siguientes errores al registrarte:</h6>
+                                {Object.keys(errors).map((key) => (
+                                    <p key={key}>{key}: {errors[key]}</p>
+                                ))}
+                            </div>
+                        )}
+
+                        <MDBInput className='mb-4' type='text' id='username' label='Nombre' />
+                        <MDBInput className='mb-4' type='email' id='email' label='Correo electrónico' />
+                        <MDBInput className='mb-4' type='password' id='password' label='Contraseña' />
+                        <MDBInput className='mb-4' type='password' id='password_confirmation' label='Confirmar contraseña' />
+                        {/* <MDBInput className='mb-4' type='tel' id='phoneNumber' label='Phone number' /> */}
+
+                        <MDBBtn type='submit' className='mb-4' block onClick={register}>
+                            Crear Usuario
+                        </MDBBtn>
+                    </form>
+                </MDBCardText>
+            </MDBCardBody>
+        </MDBCard>
+        // </div>
+    );
+}
+
+export default Register;
